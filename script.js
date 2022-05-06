ced03c47ac3a81de9b89b72c2e5629a272a544aa
Laura Gynther <lauragynther@live.com>
2022-05-06 16:41:40 +0300

    projektin aloitus

---
+++ script.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("header");
+let ctx = canvas.getContext("2d");
+ctx.font = "50px Arial";
+ctx.fillStyle = "rgb(238, 238, 114)";
+ctx.textAlign = "center";
+//ctx.strokeText("Hedelmäpeli", canvas.width/2, canvas.height/2);
+ctx.fillText("Hedelmäpeli",canvas.width/2,canvas.height/2+20);
+
+
