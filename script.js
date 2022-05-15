cb92ae92189f5298b8595465ae840080630f45be
Laura Gynther <lauragynther@live.com>
2022-05-15 18:31:26 +0300

    tarkistavoitto funktion korjausyrityksiä

--- script.js
+++ script.js
@@ -17,11 +17,14 @@ const slots = [0,1,2,3];
 const kuvat = document.querySelectorAll(".kehys");
 // Kuvat:
 const images = ['apple.png','cherry.png','number.jpg','pear.png','watermelon.png'];
-let frames = ['img1','img2','img3','img4'];
+
 let img1 = document.getElementById("kuva1");
 let img2 = document.getElementById("kuva2");
 let img3 = document.getElementById("kuva3");
 let img4 = document.getElementById("kuva4");
+
+const arvotut = [img1,img2,img3,img4];
+
 
 function asetaPanos(uusiPanos){
     panos = uusiPanos
@@ -45,19 +48,19 @@ function pelaa(uusiKierros){
 function arpoo(){
     if (locks[0] == 0){
         let luku1 = Math.floor((Math.random() * [images.length]) );
-        img1.src = `images/${[images[luku1]]}`;// luvut väliltä 0-4!
+        img1.src = `images/${[images[luku1]]}`;// luvut väliltä 0-4!*/
     }
     if (locks[1] == 0){
         let luku2 = Math.floor((Math.random() * [images.length]) );
-        img2.src = `images/${[images[luku2]]}`;// luvut väliltä 0-4!
+        img2.src = "images/apple.png"/*`images/${[images[luku2]]}`;// luvut väliltä 0-4!*/
     }
     if (locks[2] == 0){
         let luku3 = Math.floor((Math.random() * [images.length]) );
-        img3.src = `images/${[images[luku3]]}`;// luvut väliltä 0-4!
+        img3.src = "images/apple.png"/*`images/${[images[luku3]]}`;// luvut väliltä 0-4!*/
     }
     if (locks[3] == 0){
         let luku4 = Math.floor((Math.random() * [images.length]) );
-        img4.src = `images/${[images[luku4]]}`;// luvut väliltä 0-4!
+        img4.src = "images/apple.png"/*`images/${[images[luku4]]}`;// luvut väliltä 0-4!*/
     }
 }
 
@@ -71,29 +74,28 @@ function lukitse(slot){
         kuvat[slot].classList.remove("locked");
     }
 }
-// Seuraavana funktio joka tarkistaa tuliko voittoa.
 
 function tarkistaVoitto(){
-    if (images[0,0,0,0]){
+    if (arvotut[0] == images[0]){
         voitto = 6 * panos;
         tulostaVoitto();
-    } 
-    else if (images[1,1,1,1]){
+    } /*
+    else if (slots[0] == images[1] && slots[1] == images[1] && slots[2] == images[1] && slots[3] == images[1]){
         voitto = 3 * panos;
         tulostaVoitto();
     }
-    else if (images[2,2,2,2]){
+    else if (slots[0] == images[2] && slots[1] == images[2] && slots[2] == images[2] && slots[3] == images[2]){
         voitto = 10 * panos;
         tulostaVoitto();
     }
-    else if (images[3,3,3,3]){
+    else if (slots[0] == images[3] && slots[1] == images[3] && slots[2] == images[3] && slots[3] == images[3]){
         voitto = 4 * panos;
         tulostaVoitto();
     }
-    else if (slots[images[4,4,4,4]]){
+    else if (slots[0] == images[4] && slots[1] == images[4] && slots[2] == images[4] && slots[3] == images[4]){
         voitto = 5 * panos;
         tulostaVoitto();
-    }
+    }*/
     else {
         document.getElementById("voitto").innerHTML = "Ei voittoa!";
     }
